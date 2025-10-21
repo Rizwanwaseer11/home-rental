eb7f30e6a6a25c307d7bf9c35515b56a35c6bc4c
Rizwan Ahmed Waseer <rizwanwaseer98@gmail.com>
2025-10-21 14:35:53 +0500

    remove errors

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.use(limiter);
 // session configuration for login through https:
 app.set('trust proxy', 1);
 
-c
+
 // 1. Session middleware (must be first)
 app.use(session({
   secret: process.env.SESSION_SECRET || "keyboardcat",
